cb8d7f44fe715fff9b4396590984d08260f60247
Antonio Galdó <antoniogs9@hotmail.es>
2019-10-12 19:17:46 +0200

    tests

---
+++ test_random.py
@@ -0,0 +1,12 @@
+import pytest
+from hola import Hola
+
+def suma(x)
+	return x+1
+
+def test_suma():
+	assert suma(4) ==5
+
+#def test_hola():
+#	h=hola()
+#	assert isinstance(h,Hola), "Error al crear una instancia"
