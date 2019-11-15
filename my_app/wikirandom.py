6b4fea1170dc025f29f62d3c7565c5a46676c1e8
Antonio Galdó <antoniogs9@hotmail.es>
2019-11-15 13:48:08 +0100

    faltaba un archivo de Python

---
+++ my_app/wikirandom.py
@@ -0,0 +1,23 @@
+import random
+
+class RandomImage:
+
+	def __init__(self):
+		self.links = ['https://pbs.twimg.com/media/EIxOE3pWwAEGvCl?format=jpg&name=360x360','https://pbs.twimg.com/media/EIkQnbdXkAAhMl9?format=jpg&name=900x900','https://pbs.twimg.com/media/EH6OUtEXkAAFEcK?format=jpg&name=small']
+
+	def getLinks(self):
+		return self.links
+
+	def getRandomImage(self):
+		i=random.randint(0,2)
+		return self.links[i]
+
+	def pushImage(self,link):
+		self.links.append(link)
+		return 0
+
+	def getImage(self,i):
+		return self.links[i]
+
+	def getSize(self):
+		return len(self.links)
